538eaab156b31f923cf5ded0e4c0ac771dd4b224
Focix <voskresenskii.vv@gmail.com>
2020-10-14 15:19:52 +0300

    Теперь игра не заканчивается, когда лопнуты все шарики. Исправлен pep

--- catch the ball.py
+++ catch the ball.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.draw import *
 from random import randint
 import numpy as np
+
 pygame.init()
 
 miss = True
@@ -12,9 +13,8 @@ display_height = 700
 screen = pygame.display.set_mode((display_width, display_height))
 number_of_balls = 8
 
-#поверхность для вывода счета
+# поверхность для вывода счета
 score_screen = pygame.font.Font(None, 36)
-
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -25,53 +25,59 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-#список, хранящий характеристики всеъ шаров
-global pool
-pool = [[[randint(100, display_width - 100), randint(100, display_height - 100)], randint(10, 70), COLORS[randint(0, 5)],
-         randint(50, 100), randint(0, 360)] for i in range (number_of_balls)]
+# список, хранящий характеристики всеъ шаров
 # [координата x, координата y], радиус, цвет, скорость, угол
+global pool
+pool = [[[randint(100, display_width - 100),
+          randint(100, display_height - 100)],
+         randint(10, 70), COLORS[randint(0, 5)],
+         randint(50, 100), randint(0, 360)] for i in range(number_of_balls)]
+
+
 
 def move_ball():
     """
     Функция передвигает мячи
     """
-    for unit in pool:
-        bump(unit)
-        unit[0][0] += int(unit[3] / FPS * np.cos(np.pi * unit[4] / 180))
-        unit[0][1] -= int(unit[3] / FPS * np.sin(np.pi * unit[4] / 180))
-        circle(screen, unit[2], unit[0], unit[1])
+    for item in pool:
+        bump(item)
+        item[0][0] += int(item[3] / FPS * np.cos(np.pi * item[4] / 180))
+        item[0][1] -= int(item[3] / FPS * np.sin(np.pi * item[4] / 180))
+        circle(screen, item[2], item[0], item[1])
 
-def bump(unit):
+
+def bump(item):
     """
     Функция проверяет удар мячика о стенку и считает новый угол
     unit - элемент списка pool, хранящий все характеричтики данного шара
     """
-    #проверка на удар в вертикальные стены
-    if unit[0][0] + unit[1] > display_width - 5:
-        if unit[4] <= 180:
-            unit[4] = 180 - unit[4]
+    # проверка на удар в вертикальные стены
+    if item[0][0] + item[1] > display_width - 5:
+        if item[4] <= 180:
+            item[4] = 180 - item[4]
         else:
-            unit[4] = 540 - unit[4]
-    if unit[0][0] - unit[1] <= 5:
-        if unit[4] <= 180:
-            unit[4] = 180 - unit[4]
+            item[4] = 540 - item[4]
+    if item[0][0] - item[1] <= 5:
+        if item[4] <= 180:
+            item[4] = 180 - item[4]
         else:
-            unit[4] = 540 - unit[4]
-    #проверка на удар в горизонтальные стены
-    if unit[0][1] + unit[1] >= display_height - 5:
-        unit[4] = 360 - unit[4]
-    if unit[0][1] - unit[1] <= 5:
-        unit[4] = 360 - unit[4]
+            item[4] = 540 - item[4]
+    # проверка на удар в горизонтальные стены
+    if item[0][1] + item[1] >= display_height - 5:
+        item[4] = 360 - item[4]
+    if item[0][1] - item[1] <= 5:
+        item[4] = 360 - item[4]
 
 
-def score_bar(score):
+def score_bar(s):
     """
     score - значение счета игрока
     Функция вывод счет игрока в левый верхний угол экрана
     """
-    score_line = 'Score' + ':' + str(score)
+    score_line = 'Score' + ':' + str(s)
     score_text = score_screen.render(score_line, 1, (180, 0, 0))
     screen.blit(score_text, (0, 10))
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -86,14 +92,17 @@ while not finished:
             break
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for unit in pool:
-                if (event.pos[0] - unit[0][0] ) ** 2 + (event.pos[1] - unit[0][1]) ** 2 <= unit[1] ** 2:
+                if (event.pos[0] - unit[0][0]) ** 2 + \
+                        (event.pos[1] - unit[0][1]) ** 2 <= unit[1] ** 2:
                     if unit[1] <= 20:
                         score += 2
                     else:
                         score += 1
                     pool.remove(unit)
-                    pool.append([[randint(100, display_width - 100), randint(100, display_height - 100)],
-                                 randint(10, 70), COLORS[randint(0, 5)], randint(50, 100), randint(0, 360)])
+                    pool.append([[randint(100, display_width - 100),
+                                  randint(100, display_height - 100)],
+                                 randint(10, 70), COLORS[randint(0, 5)],
+                                 randint(50, 100), randint(0, 360)])
                     miss = False
                     break
             if miss:
