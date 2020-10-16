37910323fb9a9e483a938bb9a01e613612ec578d
Focix <voskresenskii.vv@gmail.com>
2020-10-16 22:53:35 +0300

    добавил список рекордов, сортировка работает хорошо

--- catch the ball.py
+++ catch the ball.py
@@ -12,8 +12,7 @@ display_width = 1200
 display_height = 700
 number_of_balls = 8
 number_of_squares = 3
-user_name = str(input('What is your name?'))
-result = user_name
+result = str(input('What is your name?\n'))
 screen = pygame.display.set_mode((display_width, display_height))
 
 # поверхность для вывода счета
@@ -28,6 +27,7 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
+
 class Ball:
 
     def __init__(self, x, y, r, v, color, angle):
@@ -39,9 +39,13 @@ class Ball:
         self.angle = angle
 
     def move(self):
+        """
+        Метод передвигает квадрат на новое местоположение
+        """
         self.x += int(self.v / FPS * np.cos(np.pi * self.angle / 180))
         self.y += int(self.v / FPS * np.sin(np.pi * self.angle / 180))
         circle(screen, self.color, [self.x, self.y], self.r)
+
 
 class Square:
 
@@ -54,32 +58,37 @@ class Square:
         self.angle = angle
 
     def move(self):
+        """
+        Метод передвигает квадрат на новое местоположение
+        """
         self.x += int(self.v / FPS * np.cos(np.pi * self.angle / 180))
         self.y += int(self.v / FPS * np.sin(np.pi * self.angle / 180))
-        rect(screen, self.color, [self.x - self.r // 2, self.y - self.r // 2, self.r, self.r])
+        rect(screen, self.color, [self.x - self.r // 2, self.y - self.r // 2,
+                                  self.r, self.r])
 
 
-def bump(ball):
+def bump(figure):
         """
         Функция проверяет удар мячика о стенку и считает новый угол
         ball - элемент класса Ball, хранящийся в списке pool
         """
         # проверка на удар в вертикальные стены
-        if ball.x + ball.r > display_width :
-            if ball.angle <= 180:
-                ball.angle = 180 - ball.angle
+        if figure.x + figure.r > display_width:
+            if figure.angle <= 180:
+                figure.angle = 180 - ball.angle
             else:
-                ball.angle = 540 - ball.angle
-        if ball.x - ball.r <= 0:
-            if ball.angle <= 180:
-                ball.angle = 180 - ball.angle
+                figure.angle = 540 - ball.angle
+        if figure.x - figure.r <= 0:
+            if figure.angle <= 180:
+                figure.angle = 180 - figure.angle
             else:
-                ball.angle = 540 - ball.angle
+                figure.angle = 540 - figure.angle
         # проверка на удар в горизонтальные стены
-        if ball.y + ball.r >= display_height:
-            ball.angle = 360 - ball.angle
-        if ball.y - ball.r <= 0:
-            ball.angle = 360 - ball.angle
+        if figure.y + figure.r >= display_height:
+            figure.angle = 360 - figure.angle
+        if figure.y - figure.r <= 0:
+            figure.angle = 360 - figure.angle
+
 
 def score_bar(s):
     """
@@ -90,35 +99,43 @@ def score_bar(s):
     score_text = score_screen.render(score_line, 1, (180, 0, 0))
     screen.blit(score_text, (0, 10))
 
-def recording(stroka, score):
-    stroka = str(score) + ' - ' + stroka
-    with open('score_board', 'r') as reading:
-        lines = [stroka]
-        for line in reading:
-            if line!='\n':
-                lines.append(line)
-        lines.sort()
-    with open('score_board', 'w') as writing:
-        for line in lines:
-            print(line, file=writing)
 
-pool = [] # список всех мячей
+def recording(s, count):
+    """
+    Функция записывает в файл 'score_board.txt' результаты игроков
+    и сортирует их
+    s - строка, содержащаяа имя игрока
+    count - результат игрока
+    """
+    s = str(count) + ' - ' + s
+    with open('score_board', 'r') as reading:
+        lines = [s.split()]
+        for line in reading:
+            if line != '\n' and line != '':
+                lines.append(line.split())
+        lines.sort(key=lambda x: x[0])
+        lines.reverse()
+    with open('score_board', 'w') as writing:
+        writing.truncate()
+        for line in lines:
+            print(' '.join(line), file=writing)
+
+
+pool = []  # список всех мячей
 
 for i in range(number_of_balls):
     ball = Ball(randint(100, display_width - 100),
-                                  randint(100, display_height - 100),
-                                 randint(10, 70), randint(50, 100),
-                                COLORS[randint(0, 5)],randint(0, 360))
+                randint(100, display_height - 100), randint(10, 70),
+                randint(50, 100), COLORS[randint(0, 5)], randint(0, 360))
     pool.append(ball)
 
 for i in range(number_of_squares):
     square = Square(randint(100, display_width - 100),
-                                  randint(100, display_height - 100),
-                                 randint(10, 70), randint(50, 100),
-                                COLORS[randint(0, 5)],randint(0, 360))
+                    randint(100, display_height - 100), randint(10, 70),
+                    randint(50, 100), COLORS[randint(0, 5)], randint(0, 360))
     pool.append(square)
 
-classes = [Ball, Square] # список классов
+classes = [Ball, Square]  # список классов
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -142,11 +159,11 @@ while not finished:
                             score += 1
                     else:
                         score += 3
-                    new_item = classes[randint(0, 1)]\
-                        (randint(100, display_width - 100),
-                                        randint(100, display_height - 100),
-                                        randint(10, 70), randint(50, 100),
-                                        COLORS[randint(0, 5)], randint(0, 360))
+                    new_item = classes[randint(0, 1)](
+                        randint(100, display_width - 100),
+                        randint(100, display_height - 100),
+                        randint(10, 70), randint(50, 100),
+                        COLORS[randint(0, 5)], randint(0, 360))
                     pool.remove(item)
                     pool.append(new_item)
                     miss = False
@@ -154,11 +171,11 @@ while not finished:
             if miss:
                 score -= 1
                 break
-        if pygame.time.get_ticks() > 20000:
-            print('You ran out of time')
-            print('Your score is', score)
-            recording(result, score)
-            finished = True
+    if pygame.time.get_ticks() > 10000:
+        print('You ran out of time')
+        print('Your score is', score)
+        recording(result, score)
+        finished = True
     for ball in pool:
         bump(ball)
         ball.move()
